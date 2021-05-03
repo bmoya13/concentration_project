22d8c877900dbe41b40a0ac0b4090d55a4f55d45
justintho <justinho282@gmail.com>
2021-05-03 11:47:19 -0700

    Created functionality that allows user to select two cards. When selected, the card will flip over. The other side of all the cards are currently blank(no animals yet). Also, implemented the tryAgainButton, which now flips back the two selected cards and allows the user to selected another pair of cards. The tryAgainButton only works after the user selects a pair of cards.

--- app/src/main/java/com/example/androidproject/SecondFragment.java
+++ app/src/main/java/com/example/androidproject/SecondFragment.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class SecondFragment extends Fragment {
+    ImageView selection1, selection2;
+    boolean card1selected, card2selected;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        card1selected = false;
+        card2selected = false;
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
@@ -28,6 +33,378 @@ public class SecondFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+        view.findViewById(R.id.cardback1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback7).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback13).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback14).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback15).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback16).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback17).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback18).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback19).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.cardback20).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!card1selected || !card2selected) {
+                    ImageView card = (ImageView) view;
+                    card.setImageResource(R.drawable.blankcard);
+                    if (!card1selected) {
+                        selection1 = card;
+                        card1selected = true;
+                    }
+                    else {
+                        selection2 = card;
+                        card2selected = true;
+                    }
+                }
+            }
+        });
+
+        view.findViewById(R.id.tryAgainButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (card1selected && card2selected) {
+                    selection1.setImageResource(R.drawable.cardback);
+                    selection2.setImageResource(R.drawable.cardback);
+                    card1selected = false;
+                    card2selected = false;
+                }
             }
         });
     }
